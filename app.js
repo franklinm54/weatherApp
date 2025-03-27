33549ee41b29f83f4da71d99896cd55701819d03
Franklin Marcelin <55675901+franklinm54@users.noreply.github.com>
2025-03-26 21:03:33 -0400

    declared varibles and added api url and id

---
+++ app.js
@@ -0,0 +1,17 @@
+let id = '9505fd1df737e20152fbd78cdb289b6a';
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
+let city = document.querySelector('.name');
+let form = document.querySelector("form");
+let temperature = document.querySelector('.temperature');
+let description = document.querySelector('.description');
+let valueSearch = document.getElementById('name');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
+let main = document.querySelector('main');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();  
+    if(valueSearch.value != ''){
+        searchWeather();
+    }
+});
